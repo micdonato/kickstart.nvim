200d3e7ed84f8d7cb6cf76bade0fc7355cc865ba
Michele Donato <michele.donato@freenome.com>
2024-09-06 10:44:28 -0700

    somehow darker material

--- init.lua
+++ init.lua
@@ -841,8 +841,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'material-darker'
-      vim.g.material_style = 'darker'
+      -- vim.cmd.colorscheme 'material-darker'
+      -- vim.g.material_style = 'darker'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
