9d27d8747f233bce85fce1495a826ebc6df90153
Michele Donato <michele.donato@freenome.com>
2024-10-23 14:12:21 -0700

    adrnote

---
+++ lua/custom/plugins/adrnote.lua
@@ -0,0 +1,25 @@
+local adrnote = {}
+
+function adrnote.setup()
+  local function open_adr_note()
+    local template_path = os.getenv 'HOME' .. '/.config/nvim/templates/adr_template.md'
+    local file = io.open(template_path, 'r')
+    if not file then
+      print('Error: Template file not found at ' .. template_path)
+      return
+    end
+
+    local content = file:read '*a'
+    file:close()
+
+    vim.api.nvim_command 'enew' -- Create a new buffer
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(content, '\n')) -- Set the buffer content
+  end
+
+  -- Create a command `:AdrNote` that calls the function
+  vim.api.nvim_create_user_command('AdrNote', open_adr_note, {})
+end
+
+return {
+  setup = adrnote.setup(),
+}
