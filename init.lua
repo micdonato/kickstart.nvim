5a9f868cf20d1341dfdc5aef8879a31059ce5954
mic <michele.donato@gmail.com>
2024-11-11 18:11:18 -0800

    neorg

--- init.lua
+++ init.lua
@@ -237,6 +237,29 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to force a plugin to be loaded.
   --
+  {
+    'nvim-neorg/neorg',
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = '*', -- Pin Neorg to the latest stable release
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {},
+          ['core.concealer'] = {},
+          ['core.dirman'] = {
+            config = {
+              workspaces = {
+                notes = '~/workspaces/notes',
+              },
+              default_workspace = 'notes',
+            },
+          },
+        },
+      }
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  },
 
   { 'marko-cerovac/material.nvim' },
 
