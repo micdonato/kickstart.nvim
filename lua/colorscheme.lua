5d514644155aa9fdb14a53c41c4ca3dcea2f85ec
Michele Donato <michele.donato@freenome.com>
2026-09-17 13:19:35 -0700

    themes

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,4 +1,27 @@
-vim.o.termguicolors = true
+vim.opt.termguicolors = true
 vim.o.background = 'dark'
-vim.cmd.colorscheme 'solarized8'
-vim.cmd.hi 'Comment gui=none'
+
+local vscode = require(vim.g.vscode and 'vscode.init' or 'vscode')
+
+vscode.setup {
+  terminal_colors = true,
+}
+
+local function use_light_theme()
+  vim.o.background = 'light'
+  vscode.load('light')
+end
+
+local function use_dark_theme()
+  vim.o.background = 'dark'
+  vscode.load('dark')
+end
+
+vim.api.nvim_create_user_command('ThemeLight', use_light_theme, { desc = 'Use VS Code light theme' })
+vim.api.nvim_create_user_command('ThemeDark', use_dark_theme, { desc = 'Use VS Code dark theme' })
+
+if vim.g.vscode then
+  vscode.load('dark')
+else
+  vim.cmd.colorscheme('vscode')
+end
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,6 +2,7 @@ local function gh(repo) return 'https://github.com/' .. repo end
 
 vim.pack.add {
   gh 'projekt0n/github-nvim-theme',
+  gh 'Mofiqul/vscode.nvim',
   gh 'cameron-wags/rainbow_csv.nvim',
   gh 'Tsuzat/NeoSolarized.nvim',
   gh 'Vigemus/iron.nvim',
